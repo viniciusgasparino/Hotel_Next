219d7aae4e45a740b1e188d94fb775ff3e1ac36f
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-09-01 12:43:24 -0300

    Criado app e  document

---
+++ pages/_app.jsx
@@ -0,0 +1,25 @@
+import {createGlobalStyle,ThemeProvider} from "styled-components"
+import theme from "../src/theme"
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+  }
+
+  body{
+    font-family: 'Roboto',sans-serif;
+  }
+
+`
+
+function App({Component,pageProps}){
+  return(
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps}/>
+    </ThemeProvider>
+  )
+}
+
+export default App
